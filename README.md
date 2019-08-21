c3e838c1bb8acc1c38d4baa976ef5363b7ddb384
Karl Aleksander Leoste <KarlAleksander.Leoste@tptlive.ee>
2019-08-21 15:52:25 +0300

    cleaned code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # h5p-math-input
 Implements Visual Math Editor for h5p in Drupal 8.
+
+# Installation
+- add module to Drupal 8 site.
+- install module
+
+# Extra steps after installation
+- Add H5P module MathDisplay
---
+++ vendor/ckplugin.js
@@ -0,0 +1,32 @@
+/* doesnt work in IE (any version) */
+var fullUrl = document.currentScript.src;
+/* doesnt work in IE (any version) */
+var folder = fullUrl.substring(0, fullUrl.lastIndexOf('/'));
+
+var H5PEditor = H5PEditor || {};
+H5PEditor.HtmlAddons = H5PEditor.HtmlAddons || {};
+H5PEditor.HtmlAddons.eqneditor = H5PEditor.HtmlAddons.eqneditor || {};
+H5PEditor.HtmlAddons.eqneditor.eqneditor = function (config, tags) {
+
+  // Add the plugin.
+  config.extraPlugins = (config.extraPlugins ? ',' : '') + 'ck_vme,iframedialog';
+
+    // Add plugin to toolbar.
+  config.toolbar.push({
+    name: "ck_vme",
+    items: ['VMELauncher']
+  });
+
+  tags.push('sup');
+  tags.push('img');
+  tags.push('eqneditor');
+};
+
+(function ($) {
+  $(document).ready(function () {
+    if (window.CKEDITOR !== undefined) {
+      CKEDITOR.plugins.addExternal('ck_vme', folder + '/ck_vme/');
+      CKEDITOR.plugins.addExternal('iframedialog', folder + '/iframedialog/');
+    }
+  });
+})(H5P.jQuery);
